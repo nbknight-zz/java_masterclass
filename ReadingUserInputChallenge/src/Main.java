f0b9eab789b179d51cc3be5e1cd2c55ebb3fd6fd
Nick <nicholas.b.knight@gmail.com>
2020-11-02 23:53:59 -0500

    User Input Challenge Section 5

---
+++ ReadingUserInputChallenge/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+
+        while(true) {
+            int order = counter + 1;
+
+        }
+
+
+        scanner.close();
+    }
+}
