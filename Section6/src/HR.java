9d88b3e08f44ee56097e62306141a658fd21b4b3
Nick <nicholas.b.knight@gmail.com>
2020-12-02 23:26:30 -0500

    Adding lesson from inheritance

---
+++ Section6/src/HR.java
@@ -0,0 +1,11 @@
+public class HR {
+    public static void main(String[] args) {
+        solution(2);
+    }
+
+    public static void solution(int N) {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(N * i);
+        }
+    }
+}
--- Section6/src/Main.java
+++ Section6/src/Main.java
@@ -95,17 +95,17 @@ public class Main {
 //        floor = new Floor(5.4, 4.5);
 //        calculator = new Calculator(floor, carpet);
 //        System.out.println("total= " + calculator.getTotalCost());
-        ComplexNumber one = new ComplexNumber(2.0, 2.0);
-        ComplexNumber number = new ComplexNumber(2.0, 2.0);
-        one.add(1,1);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        one.subtract(number);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        number.subtract(one);
-        System.out.println("number.real= " + number.getReal());
-        System.out.println("number.imaginary= " + number.getImaginary());
+//        ComplexNumber one = new ComplexNumber(2.0, 2.0);
+//        ComplexNumber number = new ComplexNumber(2.0, 2.0);
+//        one.add(1,1);
+//        System.out.println("one.real= " + one.getReal());
+//        System.out.println("one.imaginary= " + one.getImaginary());
+//        one.subtract(number);
+//        System.out.println("one.real= " + one.getReal());
+//        System.out.println("one.imaginary= " + one.getImaginary());
+//        number.subtract(one);
+//        System.out.println("number.real= " + number.getReal());
+//        System.out.println("number.imaginary= " + number.getImaginary());
 
 
     }
