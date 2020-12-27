212ce7aeba1e490b7132996e205faa72b5c61e0a
Nick <nicholas.b.knight@gmail.com>
2020-12-27 17:10:03 -0500

    Cleanup

---
+++ student work/src/CarDealerHashMap.java
@@ -0,0 +1,20 @@
+package com.tts.loops_map.entry;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.*;
+/*This project does the following: Create a hash map of vehicles.
+ * 	The model is the Key, the make is the Value.
+	Ask the customer what car (model) they are looking for.
+	Use the Hash Map to determine if you have that vehicle, and what make it is.
+	(e.g. "Oh, you're looking for a Civic? Our Honda selection is right over here...")
+ *@author Nelson T. Sherman
+ *Date: 12/4/2020
+ */
+public class CarDealerHashMap {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println(Math.pow(4,4));
+
+    }
+}
